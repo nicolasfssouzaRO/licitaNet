64615d2cfbeb8e043083139c57a2960af0636c4b
nicolasfssouzaRO <nicolasfssouza@gmail.com>
2025-04-28 19:49:40 -0400

    litando itens da contratação

--- licitacoes.js
+++ licitacoes.js
@@ -2,6 +2,7 @@ let apiUrl = "";
 let currentPage = 1; // Página inicial
 const resultsPerPage = 10; // Número de resultados por página
 let totalPages = 0;
+var responseData;
 
     // Carregar o header dinamicamente
     fetch('header.html')
@@ -32,6 +33,26 @@ document.getElementById('next-page').addEventListener('click', async function ()
     }
 });
 
+document.getElementById('close-modal').addEventListener('click', () => {
+    document.getElementById('modal-itens-edital').classList.add('hidden');
+});
+
+document.getElementById('close-modal-footer').addEventListener('click', () => {
+    document.getElementById('modal-itens-edital').classList.add('hidden');
+});
+
+
+// Chama a função para carregar as modalidades ao carregar a página
+document.addEventListener('DOMContentLoaded', carregarModalidades);
+
+document.getElementById('limpar-button').addEventListener('click', () => {
+    // Limpa o conteúdo da modal
+    const modalContent = document.getElementById('modal-content');
+    modalContent.innerHTML = '';
+
+    // Fecha a modal
+    document.getElementById('modal-itens-edital').classList.add('hidden');
+});
 async function carregarModalidades() {
     const selectModalidade = document.getElementById('search-modalidade');
 
@@ -63,9 +84,6 @@ async function carregarModalidades() {
         console.error('Erro ao carregar modalidades:', error);
     }
 }
-
-// Chama a função para carregar as modalidades ao carregar a página
-document.addEventListener('DOMContentLoaded', carregarModalidades);
 
 async function fetchAndRenderData() {
     const dataInicio = document.getElementById('search-data-inicio').value;
@@ -101,7 +119,7 @@ async function fetchAndRenderData() {
             throw new Error(`Erro na requisição: ${response.statusText}`);
         }
 
-        const responseData = await response.json();
+        responseData = await response.json();
 
         if (responseData.data && Array.isArray(responseData.data)) {
             const licitacoesResults = document.getElementById("licitacoes-results");
@@ -119,6 +137,7 @@ async function fetchAndRenderData() {
                     <div class="flex space-x-2 mt-4">
                         <p><strong>Valor Total Estimado:</strong> ${item.valorTotalEstimado?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) || 'Não informado'}</p>
                         <p><strong>Portal:</strong> ${item.usuarioNome || 'Não informado'}</p>
+                        <p><strong>Data Encerramento:</strong> ${item.dataEncerramentoProposta ? new Date(item.dataEncerramentoProposta).toLocaleDateString('pt-BR') : 'Não informado'}</p>
                     </div>
                     <div class="flex space-x-2 mt-4">
                         <button class="bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
@@ -127,19 +146,10 @@ async function fetchAndRenderData() {
                         <button class="bg-gray-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                             Arquivos
                         </button>
-                        <button class="bg-green-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" onclick="toggleItensEdital(this)">
+                        <button class="bg-green-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" data-item='${JSON.stringify(item)}' onclick="toggleItensEdital(this)">
                             Ver Itens do Edital
                         </button>
-                    </div>
-                    <div class="hidden mt-2 p-4 bg-gray-100 rounded-lg shadow-md" id="itens-edital">
-                        <p><strong>Itens do Edital:</strong></p>
-                        <ul>
-                            <li>Item 1: Descrição do item</li>
-                            <li>Item 2: Descrição do item</li>
-                            <li>Item 3: Descrição do item</li>
-                        </ul>
-                    </div>
-                `;
+                    </div>`;
 
                 licitacoesBody.appendChild(panel);
             });
@@ -155,13 +165,71 @@ async function fetchAndRenderData() {
         console.error('Erro ao buscar licitações:', error);
     }
 }
-    function toggleItensEdital(button) {
-        const itensEdital = button.nextElementSibling;
-        if (itensEdital.classList.contains('hidden')) {
-            itensEdital.classList.remove('hidden');
-            button.textContent = 'Ocultar Itens do Edital';
-        } else {
-            itensEdital.classList.add('hidden');
-            button.textContent = 'Ver Itens do Edital';
+async function toggleItensEdital(button) {
+    const modal = document.getElementById("modal-itens-edital");
+    const modalContent = document.getElementById("modal-content");
+    const item = JSON.parse(button.dataset.item);
+    // Limpa o conteúdo da modal antes de carregar os novos itens
+    modalContent.innerHTML = '<p class="text-gray-500">Carregando itens...</p>';
+    const apiUrlItem = `https://pncp.gov.br/api/pncp/v1/orgaos/${item.orgaoEntidade.cnpj}/compras/${item.anoCompra}/${item.sequencialCompra}/itens?pagina=1`;
+;
+    try {
+        // Faz a requisição à API
+        const response = await fetch(apiUrlItem, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro ao buscar itens do edital: ${response.statusText}`);
         }
+
+        const data = await response.json();
+
+        // Verifica se há itens retornados
+        if (data) {
+            let tableRows = '';
+        
+            // Itera sobre os itens usando forEach
+            data.forEach(item => {
+                tableRows += `
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 border border-gray-300">${item.numeroItem}</td>
+                        <td class="px-6 py-4 whitespace-normal text-sm text-gray-900 border border-gray-300">${item.descricao || 'Não informado'}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 border border-gray-300">${item.quantidade || 'Não informado'}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 border border-gray-300">${item.valorUnitarioEstimado?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) || 'Não informado'}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 border border-gray-300">${(item.quantidade && item.valorUnitarioEstimado) ? (item.quantidade * item.valorUnitarioEstimado).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) : 'Não informado'}</td>
+                    </tr>
+                `;
+            });
+        
+            // Insere as linhas na tabela
+            modalContent.innerHTML = `
+                <table class="min-w-full divide-y divide-gray-200 border border-gray-300">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Número</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Descrição</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Quantidade</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Valor Unitário Estimado</th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Valor Total Estimado</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        ${tableRows}
+                    </tbody>
+                </table>
+            `;
+        } else {
+            modalContent.innerHTML = '<p class="text-gray-500">Nenhum item encontrado.</p>';
+        }
+    } catch (error) {
+        console.error('Erro ao buscar itens do edital:', error);
+        modalContent.innerHTML = '<p class="text-red-500">Erro ao carregar os itens. Tente novamente mais tarde.</p>';
     }
+
+    // Exibe a modal
+    modal.classList.remove("hidden");
+}
